3fe974f164489474e137b58f7bb4ab6c78be847e
SirKury <aekuryr@gmail.com>
2026-08-10 20:27:54 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "optimizador-tarjetas-v1-9";
+const CACHE_NAME = "optimizador-tarjetas-v1-10";
 const APP_SHELL = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",event=>{
